67cb4a32b8cf186684d00608c7dfbae2aac6df04
Julio Afonso <julioafonsso@gmail.com>
2017-01-04 12:26:19 -0200

    Acerto Mapeamento

--- src/main/java/br/com/julios/ccc/domains/ModalidadeTurma.java
+++ src/main/java/br/com/julios/ccc/domains/ModalidadeTurma.java
@@ -1,16 +1,11 @@
 package br.com.julios.ccc.domains;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "modalidade_turma")
@@ -24,10 +19,6 @@ public class ModalidadeTurma {
 	@Column
 	private String nome;
 	
-	@OneToMany(mappedBy = "modalidade")
-	@JsonIgnoreProperties("modalidade")
-	private List<Turma> turma;
-
 	public int getId() {
 		return id;
 	}
@@ -38,14 +29,5 @@ public class ModalidadeTurma {
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-
-	public List<Turma> getTurma() {
-		return turma;
-	}
-
-	public void setTurma(List<Turma> turma) {
-		this.turma = turma;
 	}
 }
