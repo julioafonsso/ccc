30bc0aae7b9345d500c9006bfd389d3fb913a5db
Julio Afonso <julioafonsso@gmail.com>
2017-01-07 14:05:08 -0200

    Acerto CrossDomain

--- src/main/java/br/com/julios/ccc/Application.java
+++ src/main/java/br/com/julios/ccc/Application.java
@@ -24,7 +24,7 @@ public class Application {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**/*").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**/*").allowedMethods("GET", "POST","DELETE", "POST").allowedOrigins("http://localhost:4200");
             }
         };
     }
