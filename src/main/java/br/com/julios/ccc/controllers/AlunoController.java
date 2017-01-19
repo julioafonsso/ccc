9eb4c59833d10a69949f7d80978f54b131b0d015
Julio Afonso <julioafonsso@gmail.com>
2017-01-19 11:54:50 -0200

    inclusao dos metodos para pagamento de mensalidade

--- src/main/java/br/com/julios/ccc/controllers/AlunoController.java
+++ src/main/java/br/com/julios/ccc/controllers/AlunoController.java
@@ -1,7 +1,6 @@
 package br.com.julios.ccc.controllers;
 
-
-
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.julios.ccc.domains.Aluno;
+import br.com.julios.ccc.domains.Matricula;
+import br.com.julios.ccc.domains.Mensalidades;
 import br.com.julios.ccc.negocio.AlunoApi;
 
 @Controller
@@ -23,17 +24,12 @@ public class AlunoController {
 	@Autowired
 	AlunoApi alunoApi;
 
-	@RequestMapping( method = RequestMethod.GET)
-	public Iterable<Aluno> getAlunos(
-			@RequestParam(value = "nome", required =false) String nome, @RequestParam(value = "cpf" , required =false) String cpf, @RequestParam(value = "email" , required =false) String email) {
+	@RequestMapping(method = RequestMethod.GET)
+	public Iterable<Aluno> getAlunos(@RequestParam(value = "nome", required = false) String nome,
+			@RequestParam(value = "cpf", required = false) String cpf,
+			@RequestParam(value = "email", required = false) String email) {
 
 		return alunoApi.getAlunos(nome, cpf, email);
-	}
-	
-	@RequestMapping(value="{id}", method = RequestMethod.GET)
-	public Aluno getAluno(@PathVariable("id") Long idAluno)
-	{
-		return alunoApi.getAluno(idAluno);
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
@@ -50,4 +46,25 @@ public class AlunoController {
 	public void apagarAluno(Aluno aluno) {
 		alunoApi.apagarAluno(aluno);
 	}
+
+	@RequestMapping(value ="pagamento", method= RequestMethod.POST)
+	public void efetuarPagamento(@RequestBody Mensalidades mensalidade){
+		alunoApi.efetuarPagamento(mensalidade);
+	}
+	
+	@RequestMapping(value = "{id}", method = RequestMethod.GET)
+	public Aluno getAluno(@PathVariable("id") Long idAluno) {
+		return alunoApi.getAluno(idAluno);
+	}
+
+	@RequestMapping(value = "{id}/turmas", method = RequestMethod.GET)
+	public List<Matricula> getTurmas(@PathVariable("id") Long idAluno) {
+		return alunoApi.getTurmas(idAluno);
+	}
+
+	@RequestMapping(value = "{id}/debitos", method = RequestMethod.GET)
+	public List<Mensalidades> getDebitos(@PathVariable("id") Long idAluno) {
+		return alunoApi.getMensalidadesParaPagar(idAluno);
+	}
+
 }
--- src/main/java/br/com/julios/ccc/negocio/AlunoApi.java
+++ src/main/java/br/com/julios/ccc/negocio/AlunoApi.java
@@ -1,5 +1,9 @@
 package br.com.julios.ccc.negocio;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
@@ -8,7 +12,19 @@ import org.springframework.stereotype.Service;
 
 import br.com.julios.ccc.Util;
 import br.com.julios.ccc.daos.AlunoDAO;
+import br.com.julios.ccc.daos.FluxoCaixaDAO;
+import br.com.julios.ccc.daos.MatriculaDAO;
+import br.com.julios.ccc.daos.MensalidadesDAO;
+import br.com.julios.ccc.daos.MensalidadesPagasDAO;
+import br.com.julios.ccc.daos.MesReferenciaDAO;
+import br.com.julios.ccc.daos.TipoFluxoCaixaDAO;
 import br.com.julios.ccc.domains.Aluno;
+import br.com.julios.ccc.domains.FluxoCaixa;
+import br.com.julios.ccc.domains.Matricula;
+import br.com.julios.ccc.domains.Mensalidades;
+import br.com.julios.ccc.domains.MensalidadesPagas;
+import br.com.julios.ccc.domains.MesReferencia;
+import br.com.julios.ccc.domains.TipoFluxoCaixa;
 
 @Service
 public class AlunoApi {
@@ -16,6 +32,23 @@ public class AlunoApi {
 	@Autowired
 	AlunoDAO alunoDAO;
 	
+	@Autowired 
+	MensalidadesDAO mensalidadeDAO;
+
+	@Autowired
+	MensalidadesPagasDAO mensalidadesPagasDAO;
+	
+	@Autowired
+	TipoFluxoCaixaDAO tipoFluxoDAO;
+	
+	@Autowired 
+	FluxoCaixaDAO fluxoCaixaDAO;
+	
+	@Autowired
+	MesReferenciaDAO mesReferenciaDAO;
+	
+	@Autowired
+	MatriculaDAO matriculaDAO;
 	
 	public void cadastrarAluno(Aluno aluno)
 	{
@@ -56,6 +89,41 @@ public class AlunoApi {
 
 	public Aluno getAluno(Long idAluno) {
 		return alunoDAO.findOne(idAluno);	
+		
+	}
+
+	public List<Matricula> getTurmas(Long idAluno) {
+		Aluno a = alunoDAO.findOne(idAluno);
+		return a.getMatriculas();
+	}
+
+	public List<Mensalidades> getMensalidadesParaPagar(Long idAluno) {
+		List<String> situacoes = new ArrayList<String>();
+		situacoes.add(Mensalidades.PAGO);
+		return mensalidadeDAO.findByAlunoAndSituacaoNotIn(idAluno, situacoes);
+	}
+
+	public void efetuarPagamento(Mensalidades mensalidade) {
+		
+		TipoFluxoCaixa tipoFluxoMensalidade = tipoFluxoDAO.findOne(TipoFluxoCaixa.MENSALIDADE);
+		
+		FluxoCaixa fluxo = new FluxoCaixa();
+		fluxo.setDataFluxo(new Date());
+		fluxo.setDescricao("Recebimento Mensalidade ");
+		fluxo.setTipoFluxo(tipoFluxoMensalidade);
+		fluxo.setValor(mensalidade.getValorParaPagar());
+		
+		fluxoCaixaDAO.save(fluxo);
+		MensalidadesPagas mensalidadePaga = new MensalidadesPagas();
+		
+		Matricula matricula = matriculaDAO.findOne(mensalidade.getMatricula());
+		MesReferencia mes = mesReferenciaDAO.findOne(mensalidade.getMes());
+		
+		mensalidadePaga.setFluxoCaixa(fluxo);
+		mensalidadePaga.setMatricula(matricula);
+		mensalidadePaga.setMesReferencia(mes);
+		
+		mensalidadesPagasDAO.save(mensalidadePaga);
 		
 	}
 	
